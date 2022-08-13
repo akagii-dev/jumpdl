36d0fc4e01bf16b7e6bcedb7297f5f98636a72c8
muffin1000 <97124098+muffin1000@users.noreply.github.com>
2022-08-14 01:10:08 +0900

    Update trimSize.py

--- src/trimSize.py
+++ src/trimSize.py
@@ -1,15 +1,19 @@
 trimSizeList = [["少年ジャンプ＋", 1184, 800]
+                ,["少年ジャンプ", 1184, 736]
                 , ["サンデーうぇぶり", 2048, 1408]
                 ,["サンデーうぇぶり", 2048, 1440]
                 , ["となりのヤングジャンプ", 1120, 800]]
 
 def getTrimSize(mangaName, pageW):
     if mangaName == "少年ジャンプ＋":
-        return trimSizeList[0][1], trimSizeList[0][2]
+        if pageW == 822:
+            return trimSizeList[0][1], trimSizeList[0][2]
+        elif pageW == 764:
+            return trimSizeList[1][1], trimSizeList[1][2]
     elif mangaName == "サンデーうぇぶり":
         if pageW == 1426:
-            return trimSizeList[1][1], trimSizeList[1][2]
-        elif pageW == 1445:
             return trimSizeList[2][1], trimSizeList[2][2]
+        elif pageW == 1445:
+            return trimSizeList[3][1], trimSizeList[3][2]
     elif mangaName == "となりのヤングジャンプ":
         return trimSizeList[3][1], trimSizeList[3][2]
